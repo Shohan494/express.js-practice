b13b887b472b81bbc62b61d7157bab354bb43e02
Shohan494 <shohan12224943@gmail.com>
2017-06-05 14:16:53 +0600

    middleware next() function is working in router

--- routes/birds.js
+++ routes/birds.js
@@ -2,17 +2,24 @@ var express = require('express')
 var router = express.Router()
 
 // middleware that is specific to this router
+// every time the app calls from this router this function runs
+
+
 router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
+  req.timeLog = Date.now()
   next()
 })
+
 // define the home page route
 router.get('/', function (req, res) {
   res.send('Birds home page')
 })
+
 // define the about route
-router.get('/about', function (req, res) {
-  res.send('About birds')
+router.get('/about', function (req, res, next) {
+  var responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.timeLog + '</small>'
+  res.send(responseText)
 })
 
 module.exports = router
