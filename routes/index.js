237f901c61207bf3425e13f2a9ed20c2736f4623
Shohan494 <shohan12224943@gmail.com>
2017-06-05 13:11:54 +0600

    basic commenting done about route path variable included in app.js and sending data to pug template

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // rndering the data to pug template
   res.render('index', { title: 'Express' });
 });
 
